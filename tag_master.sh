3096f35ca07acd324b991f14600cb1e0837293c1
Guillermo <guillermo@bitprim.org>
2018-07-03 18:38:13 -0300

    Git user and email

--- tag_master.sh
+++ tag_master.sh
@@ -6,10 +6,14 @@ tag_master() {
 		cd "${i}"
 		git checkout master
 		git pull
+
+		git config user.email "guillermo@bitprim.org"
+		git config user.name "Hanchon"
+
 		git tag -a v"${version}" -m "version ${version}"
 		# git push --tags
-        git tag -n5
-        git show-ref --tags
+		git tag -n5
+		git show-ref --tags
 		cd ..
 	done
 }
